93dd43a3cd6571d63e7c6e9f1ce2bfd9d71061de
Krishna Vamsi Katta <83060150+Vamsi759@users.noreply.github.com>
2026-01-08 16:42:13 +0530

    Update StudentDto.java

--- src/main/java/emp1/demo/StudentDto/StudentDto.java
+++ src/main/java/emp1/demo/StudentDto/StudentDto.java
@@ -5,10 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-
 public class StudentDto {
     private Long id;
     private String name;
